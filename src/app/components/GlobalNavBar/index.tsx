15519e7d81849df0ebb39c0e38b157891d14aa5b
Bryan Romero <Bryanromerou@gmail.com>
2023-01-27 16:44:27 -0800

    small nav bar styling changes

--- src/app/components/GlobalNavBar/index.tsx
+++ src/app/components/GlobalNavBar/index.tsx
@@ -6,18 +6,22 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-import PlaceHolderIcon from "resources/images/Criteria-Logo.png";
+import PlaceHolderIcon from "resources/images/criteria-logo.png";
 
 interface Props {}
 
 export default function GlobalNavBar(props: Props) {
   return (
-    <Navbar className="global-nav-bar" variant="dark">
+    <Navbar className="global-nav-bar py-0" variant="dark">
       <Container fluid>
         <Navbar.Brand href="/">
-          <img src={PlaceHolderIcon} alt="Criteria" />
+          <img
+            src={PlaceHolderIcon}
+            alt="Criteria"
+            style={{ width: "110px", height: "27px" }}
+          />
         </Navbar.Brand>
-        <Nav>
+        <Nav className="me-auto">
           <Nav.Link as={NavLink} to="/">
             My Dashboard
           </Nav.Link>
