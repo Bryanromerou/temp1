1c0648dd8778485b771b1b772e6d55b6fcab2d77
Bryan Romero <Bryanromerou@gmail.com>
2023-01-27 17:40:43 -0800

    added dummy auth

--- src/app/containers/App.tsx
+++ src/app/containers/App.tsx
@@ -1,23 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import GlobalNavBar from "app/components/GlobalNavBar";
 import Dashboard from "app/containers/Dashboard";
 import AdminConsole from "app/containers/AdminConsole";
 import TeamGuide from "app/containers/TeamGuide";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+
+// Temporary Work
+const tempPassword = "TIGER_TEAM";
 
 export default function App() {
+  // -------- Temporary Work  -----
+  const [error, setError] = useState<"Wrong Password" | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const checkPassword = e => {
+    e.preventDefault();
+    if (password === tempPassword) {
+      setIsAuthenticated(true);
+    } else {
+      setError("Wrong Password");
+    }
+  };
+
   return (
     <BrowserRouter>
-      <GlobalNavBar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/AdminConsole" element={<AdminConsole />} />
-        <Route path="/TeamGuide" element={<TeamGuide />} />
-        {/* Using path="*"" means "match anything", so this route
+      {isAuthenticated ? (
+        <>
+          <GlobalNavBar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/AdminConsole" element={<AdminConsole />} />
+            <Route path="/TeamGuide" element={<TeamGuide />} />
+            {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
               routes for. */}
-        <Route path="*" element={<div>404 Page Not Found</div>} />
-      </Routes>
+            <Route path="*" element={<div>404 Page Not Found</div>} />
+          </Routes>
+        </>
+      ) : (
+        <Form onSubmit={checkPassword}>
+          {error}
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={e => {
+                setError(null);
+                setPassword(e.target.value);
+              }}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      )}
     </BrowserRouter>
   );
 }
